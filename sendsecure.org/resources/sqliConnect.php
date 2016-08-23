054a6d5fc925444c6456937c182ea3b4784f995b
Chris Wheeler <grintor@gmail.com>
2016-08-23 20:08:53 +0000

    added mysqli timout setting

--- sendsecure.org/resources/sqliConnect.php
+++ sendsecure.org/resources/sqliConnect.php
@@ -1,14 +1,20 @@
 <?php
 
-$mysqli = new mysqli('127.0.0.1', 'username', 'password', 'sendsecure');
-if ($mysqli->connect_errno) {
-    echo 'could not contact DB';
-    exit;
+$mysqli = mysqli_init();
+if (!$mysqli) {
+    die('mysqli_init failed');
+}
+
+if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 600)) {
+    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+}
+
+if (!$mysqli->real_connect('127.0.0.1', 'user', 'pass', 'sendsecure')) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
 
 if (!$sqlResult = $mysqli->query('SET @@session.block_encryption_mode = "aes-256-cbc"')) {
-    echo 'Failed to initilize encryption';
-    exit;
+    die('Failed to initilize encryption');
 }
 
 ?>
